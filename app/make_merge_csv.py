21f98a724582429ad14561653f6d1516eee185b1
okdyy75 <okdyy75@gmail.com>
2020-11-22 00:51:46 +0900

    マージCSV作成処理修正

--- app/make_merge_csv.py
+++ app/make_merge_csv.py
@@ -1,26 +1,72 @@
-import csv
 import datetime
 import glob
-import os
 import pandas as pd
+
+"""
+共通変数・定数設定
+"""
+DATA_PATH = 'data'
+CATEGORIES = {
+    'Language',
+    'FrameWork',
+    'DB',
+    'OS',
+    'Cloud'
+}
+now = datetime.datetime.today()
 
 """
  メイン処理
 """
 def main() -> None:
 
-    files = glob.glob('data/freelance-start/*.csv')
+    filter_date = now.strftime("%Y%m%d")
+    filter_date = 20201121
 
+    # レバテックのスキル別案件集計CSVをマージする
+    # カテゴリ別で集計
+    for category_key in CATEGORIES:
+        files = glob.glob(DATA_PATH + '/levtech/' + category_key + '/*.csv')
+        putfile = category_key + '.csv'
+        list = []
+        for file in files:
+            if putfile in file:
+                continue
+
+            df = pd.read_csv(file, index_col='created_at')
+            list.append(df.loc[[filter_date]])
+
+        df = pd.concat(list, sort=False)
+        df.to_csv(DATA_PATH + '/levtech/' + putfile)
+
+    # 全体集計
+    files = glob.glob(DATA_PATH + '/levtech/*.csv')
+    putfile = 'levtech.csv'
     list = []
     for file in files:
-        if 'all.csv' in file:
-            continue 
-        
+        if putfile in file:
+            continue
+
         df = pd.read_csv(file, index_col='created_at')
-        list.append(df.loc[[20201119]])
+        list.append(df.loc[[filter_date]])
 
     df = pd.concat(list, sort=False)
-    df.to_csv('data/freelance-start/all.csv')
+    df.to_csv(DATA_PATH + '/' + putfile)
+
+    # フリーランススタートのスキル別案件集計CSVをマージする
+    # 全体集計
+    files = glob.glob(DATA_PATH + '/freelance-start/*.csv')
+    putfile = 'freelance-start.csv'
+    list = []
+    for file in files:
+        if putfile in file:
+            continue
+
+        df = pd.read_csv(file, index_col='created_at')
+        list.append(df.loc[[filter_date]])
+
+    df = pd.concat(list, sort=False)
+    df.to_csv(DATA_PATH + '/' + putfile)
 
 
 if __name__ == "__main__":
