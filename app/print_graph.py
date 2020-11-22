59f8c12e7b9fe77fac1f7630de5b3cb735126c3c
okdyy75 <okdyy75@gmail.com>
2020-11-22 20:13:13 +0900

    フリーランススタートcsvのグラフ出力追加

--- app/print_graph.py
+++ app/print_graph.py
@@ -95,6 +95,7 @@ def count_graph(make_date: str) -> None:
 """
 def mix_graph(make_date: str) -> None:
 
+    # レバテック
     for category_key in CATEGORIES:
         df = pd.read_csv(DATA_PATH + '/levtech/' + category_key + '.csv')
         df = df.sort_values(by=['avg_price'], ascending=False)
@@ -134,6 +135,46 @@ def mix_graph(make_date: str) -> None:
 
         # plt.show()
         fig.savefig(DATA_PATH + '/graphs/' + make_date + '/levtech-' + category_key + '-mix.png', bbox_inches='tight', format='png', dpi=300)
+
+        # フリーランススタート
+        df = pd.read_csv(DATA_PATH + '/freelance-start.csv')
+        df = df.sort_values(by=['avg_price'], ascending=False)
+
+        labels = df['skill'].str.replace('[ぁ-んァ-ン一-龥]+', '', regex=True)  # 日本語削除
+        y_list1 = df['avg_price']
+        y_list2 = df['count']
+
+        fig = plt.figure(figsize=(14, 7))
+
+        left = np.arange(len(labels))
+        width = 0.3
+
+        ax1 = fig.add_subplot(1, 1, 1)
+        ax1.bar(labels, y_list1, width=width, color='tab:blue', label='price')
+        ax1.set_ylabel('price')
+        ax1.set_title(category_key)
+
+        ax2 = ax1.twinx()
+        ax2.bar(left + width, y_list2, width=width, color='tab:orange', label='count')
+        ax2.set_ylabel('count')
+
+        # X軸を縦書き表示
+        ax1.set_xticklabels(labels, rotation=90)
+
+        # 棒グラフに数値追記
+        for x, y in zip(labels, y_list1):
+            ax1.text(x, y, y, ha='center', va='bottom', fontsize='xx-small')
+
+        for x, y in zip(left + width, y_list2):
+            ax2.text(x, y, y, ha='center', va='bottom', fontsize='xx-small')
+
+        # 凡例を出力
+        handler1, label1 = ax1.get_legend_handles_labels()
+        handler2, label2 = ax2.get_legend_handles_labels()
+        ax1.legend(handler1 + handler2, label1 + label2)
+
+        # plt.show()
+        fig.savefig(DATA_PATH + '/graphs/' + make_date + '/freelance-start-mix.png', bbox_inches='tight', format='png', dpi=300)
 
 
 if __name__ == '__main__':
