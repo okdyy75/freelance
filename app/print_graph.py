9927fc01308ef9f05007154d7bf27c1cef846381
okdyy75 <okdyy75@gmail.com>
2020-11-23 18:56:04 +0900

    フリーランススタートの単価・案件数のグラフ追加。単価が小数点なので四捨五入に修正

--- app/print_graph.py
+++ app/print_graph.py
@@ -37,6 +37,7 @@ def main() -> None:
 """
 def price_graph(make_date: str) -> None:
 
+    """ レバテック """
     for category_key in CATEGORIES:
         df = pd.read_csv(DATA_PATH + '/levtech/' + category_key + '.csv')
         df = df.sort_values(by=['avg_price'], ascending=False)
@@ -60,12 +61,35 @@ def price_graph(make_date: str) -> None:
         # plt.show()
         fig.savefig(DATA_PATH + '/graphs/' + make_date + '/levtech-' + category_key + '-price.png', bbox_inches='tight', format='png', dpi=300)
 
+    """ フリーランススタート """
+    df = pd.read_csv(DATA_PATH + '/freelance-start.csv')
+    df = df.sort_values(by=['avg_price'], ascending=False)
+    labels = df['skill'].str.replace('[ぁ-んァ-ン一-龥]+', '', regex=True)  # 日本語削除
+    y_list1 = df['avg_price'].round().astype(int)
+
+    fig = plt.figure(figsize=(14, 7))
+
+    ax1 = fig.add_subplot(1, 1, 1)
+    ax1.bar(labels, y_list1, color='tab:blue')
+    ax1.set_ylabel('price')
+    ax1.set_title(category_key)
+
+    # X軸を縦書き表示
+    ax1.set_xticklabels(labels, rotation=90)
+
+    # 棒グラフに数値追記
+    for x, y in zip(labels, y_list1):
+        ax1.text(x, y, y, ha='center', va='bottom', fontsize='xx-small')
+
+    # plt.show()
+    fig.savefig(DATA_PATH + '/graphs/' + make_date + '/freelance-start-price.png', bbox_inches='tight', format='png', dpi=300)
 
 """
  案件数グラフ作成
 """
 def count_graph(make_date: str) -> None:
 
+    """ レバテック """
     for category_key in CATEGORIES:
         df = pd.read_csv(DATA_PATH + '/levtech/' + category_key + '.csv')
         df = df.sort_values(by=['count'], ascending=False)
@@ -89,13 +113,35 @@ def count_graph(make_date: str) -> None:
         # plt.show()
         fig.savefig(DATA_PATH + '/graphs/' + make_date + '/levtech-' + category_key + '-count.png', bbox_inches='tight', format='png', dpi=300)
 
+    """ フリーランススタート """
+    df = pd.read_csv(DATA_PATH + '/freelance-start.csv')
+    df = df.sort_values(by=['count'], ascending=False)
+    labels = df['skill'].str.replace('[ぁ-んァ-ン一-龥]+', '', regex=True)  # 日本語削除
+    y_list1 = df['count']
+
+    fig = plt.figure(figsize=(14, 7))
+
+    ax1 = fig.add_subplot(1, 1, 1)
+    ax1.bar(labels, y_list1, color='tab:orange')
+    ax1.set_ylabel('count')
+    ax1.set_title(category_key)
+
+    # X軸を縦書き表示
+    ax1.set_xticklabels(labels, rotation=90)
+
+    # 棒グラフに数値追記
+    for x, y in zip(labels, y_list1):
+        ax1.text(x, y, y, ha='center', va='bottom', fontsize='xx-small')
+
+    # plt.show()
+    fig.savefig(DATA_PATH + '/graphs/' + make_date + '/freelance-start-count.png', bbox_inches='tight', format='png', dpi=300)
 
 """
  単価＆案件数グラフ作成
 """
 def mix_graph(make_date: str) -> None:
 
-    # レバテック
+    """ レバテック """
     for category_key in CATEGORIES:
         df = pd.read_csv(DATA_PATH + '/levtech/' + category_key + '.csv')
         df = df.sort_values(by=['avg_price'], ascending=False)
@@ -136,45 +182,45 @@ def mix_graph(make_date: str) -> None:
         # plt.show()
         fig.savefig(DATA_PATH + '/graphs/' + make_date + '/levtech-' + category_key + '-mix.png', bbox_inches='tight', format='png', dpi=300)
 
-        # フリーランススタート
-        df = pd.read_csv(DATA_PATH + '/freelance-start.csv')
-        df = df.sort_values(by=['avg_price'], ascending=False)
+    """ フリーランススタート """
+    df = pd.read_csv(DATA_PATH + '/freelance-start.csv')
+    df = df.sort_values(by=['avg_price'], ascending=False)
 
-        labels = df['skill'].str.replace('[ぁ-んァ-ン一-龥]+', '', regex=True)  # 日本語削除
-        y_list1 = df['avg_price']
-        y_list2 = df['count']
+    labels = df['skill'].str.replace('[ぁ-んァ-ン一-龥]+', '', regex=True)  # 日本語削除
+    y_list1 = df['avg_price'].round().astype(int)
+    y_list2 = df['count']
 
-        fig = plt.figure(figsize=(14, 7))
+    fig = plt.figure(figsize=(14, 7))
 
-        left = np.arange(len(labels))
-        width = 0.3
+    left = np.arange(len(labels))
+    width = 0.3
 
-        ax1 = fig.add_subplot(1, 1, 1)
-        ax1.bar(labels, y_list1, width=width, color='tab:blue', label='price')
-        ax1.set_ylabel('price')
-        ax1.set_title(category_key)
+    ax1 = fig.add_subplot(1, 1, 1)
+    ax1.bar(labels, y_list1, width=width, color='tab:blue', label='price')
+    ax1.set_ylabel('price')
+    ax1.set_title(category_key)
 
-        ax2 = ax1.twinx()
-        ax2.bar(left + width, y_list2, width=width, color='tab:orange', label='count')
-        ax2.set_ylabel('count')
+    ax2 = ax1.twinx()
+    ax2.bar(left + width, y_list2, width=width, color='tab:orange', label='count')
+    ax2.set_ylabel('count')
 
-        # X軸を縦書き表示
-        ax1.set_xticklabels(labels, rotation=90)
+    # X軸を縦書き表示
+    ax1.set_xticklabels(labels, rotation=90)
 
-        # 棒グラフに数値追記
-        for x, y in zip(labels, y_list1):
-            ax1.text(x, y, y, ha='center', va='bottom', fontsize='xx-small')
+    # 棒グラフに数値追記
+    for x, y in zip(labels, y_list1):
+        ax1.text(x, y, y, ha='center', va='bottom', fontsize='xx-small')
 
-        for x, y in zip(left + width, y_list2):
-            ax2.text(x, y, y, ha='center', va='bottom', fontsize='xx-small')
+    for x, y in zip(left + width, y_list2):
+        ax2.text(x, y, y, ha='center', va='bottom', fontsize='xx-small')
 
-        # 凡例を出力
-        handler1, label1 = ax1.get_legend_handles_labels()
-        handler2, label2 = ax2.get_legend_handles_labels()
-        ax1.legend(handler1 + handler2, label1 + label2)
+    # 凡例を出力
+    handler1, label1 = ax1.get_legend_handles_labels()
+    handler2, label2 = ax2.get_legend_handles_labels()
+    ax1.legend(handler1 + handler2, label1 + label2)
 
-        # plt.show()
-        fig.savefig(DATA_PATH + '/graphs/' + make_date + '/freelance-start-mix.png', bbox_inches='tight', format='png', dpi=300)
+    # plt.show()
+    fig.savefig(DATA_PATH + '/graphs/' + make_date + '/freelance-start-mix.png', bbox_inches='tight', format='png', dpi=300)
 
 
 if __name__ == '__main__':
